b79144ce8eb0626092ed029758ebef1baae5e4c7
bsscrh <442484635@qq.com>
2020-03-18 10:41:54 +0800

    8.8

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -10,27 +10,38 @@
 			</swiper-item>
 		</swiper>
 		<!-- 轮播图end -->
-		
+
 		<!-- 热门超英start -->
-		<view class="page-block">
-			<view>
-				<image src="" mode=""></image>
-				<view>
-					
+		<view class="page-block super-hot">
+			<view class="hot-title-wapper">
+				<image src="../../static/icos/hot.png" class="hot-ico"></image>
+				<view class="hot-title">
+					热门超英
 				</view>
 			</view>
 		</view>
+		
+		<scroll-view scroll-x="true" class="page-block hot">
+			<view class="single-poster">
+				<view class="poster-wapper">
+					<image src="../../static/poster/civilwar.jpg" class="poster"></image>
+					<view class="movie-name">
+						蝙蝠侠蝙蝠侠蝙蝠侠蝙蝠侠
+					</view>
+				</view>
+			</view>
+		</scroll-view>
 		<!-- 热门超英end -->
 	</view>
 </template>
 
 <script>
 	import common from "../../common/common.js";
-	
+
 	export default {
 		data() {
 			return {
-				
+
 			}
 		},
 		onLoad() {
